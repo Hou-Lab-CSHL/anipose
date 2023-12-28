3d216a8f6f9e61b773bf35647c902edffff690e0
Kyle Daruwalla <daruwal@cshl.edu>
2023-12-28 13:28:00 -0500

    More changes to how we map bodyparts to colors

--- anipose/label_videos.py
+++ anipose/label_videos.py
@@ -10,10 +10,12 @@ from tqdm import trange
 import itertools
 
 from matplotlib import colors
-from matplotlib.pyplot import get_cmap
 from seaborn import color_palette
 
 from .common import make_process_fun, natural_keys, get_nframes
+
+def cluster_name(cluster_points):
+    return "__".join(cluster_points)
 
 def tiny_cmap(center, delta, n, cmap = "colorblind"):
     if isinstance(cmap, str):
@@ -24,7 +26,10 @@ def tiny_cmap(center, delta, n, cmap = "colorblind"):
     if isinstance(_cmap, list):
         _cmap = colors.LinearSegmentedColormap.from_list("tiny_cmap", _cmap)
 
-    return list(_cmap(np.linspace(center - delta, center + delta, n), bytes=True))
+    color_range = np.linspace(center - delta, center + delta, n)
+
+    return {"center": _cmap(center, bytes=True),
+            "points": list(_cmap(color_range, bytes=True))}
 
 def color_scheme(scheme, cmap="colorblind", spread=0.02):
     bp_to_color = {}
@@ -32,8 +37,10 @@ def color_scheme(scheme, cmap="colorblind", spread=0.02):
     for idx, cluster in enumerate(scheme):
         cluster_size = len(cluster)
         cluster_cmap = tiny_cmap(cluster_centers[idx], spread, cluster_size, cmap)
+        bp_to_color[cluster_name(cluster)] = [int(c) for c in cluster_cmap["center"]]
         for bp in cluster:
-            bp_to_color[bp] = [int(c) for c in cluster_cmap.pop()]
+            if not bp in bp_to_color:
+                bp_to_color[bp] = [int(c) for c in cluster_cmap["points"].pop()]
     return bp_to_color
 
 def connect(img, points, bps, bodyparts, col=(0,255,0,255)):
@@ -51,7 +58,7 @@ def connect(img, points, bps, bodyparts, col=(0,255,0,255)):
 
 def connect_all(img, points, scheme, bodyparts, bp_to_color):
     for bps in scheme:
-        col = bp_to_color[bps[0]]
+        col = bp_to_color[cluster_name(bps)]
         connect(img, points, bps, bodyparts, col)
 
 
@@ -112,8 +119,9 @@ def visualize_labels(config, labels_fname, vid_fname, outname):
 
     last = len(dlabs)
 
-    cmap = get_cmap(config['labeling'].get('colormap', 'tab10'))
-    bp_to_color = color_scheme(scheme, cmap=cmap)
+    cmap = config['labeling'].get('colormap', 'colorblind')
+    spread = config['labeling'].get('color_spread', 0.02)
+    bp_to_color = color_scheme(scheme, cmap=cmap, spread=spread)
 
     points = [(dlabs[bp]['x'], dlabs[bp]['y']) for bp in bodyparts]
     points = np.array(points)
